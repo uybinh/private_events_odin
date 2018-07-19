7d0e4038a006b04e3d71e0e143af870982d1bec8
Uy Binh <uybinh@gmail.com>
2018-07-19 21:04:16 +0700

    Add user show

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(name: "Binh Phan", email: "binh@gmail.com",
+                                      password: "foobar",
+                                      password_confirmation: "foobar")
+  end
+
+  test "should be valid" do
+
+  end
 end
