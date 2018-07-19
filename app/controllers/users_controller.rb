bf47ebb698bcd3e8e2ff066552975b4a0fa09878
Uy Binh <uybinh@gmail.com>
2018-07-19 20:49:09 +0700

    Edit sticky footer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  
+  before_action :signed_in_user, except: [:new, :create]
+
   def new
     @user = User.new
   end
@@ -17,10 +18,18 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = current_user
   end
 
   private
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    def signed_in_user
+      unless signed_in?
+        flash[:info] = "Please sign in first"
+        redirect_to signin_path
+      end
     end
 end
