932ce3b8d7e06ae3fd066e5d5fde53e3260ceb87
Uy Binh <uybinh@gmail.com>
2018-07-19 14:36:41 +0700

    Add seed data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_19_073046) do
+ActiveRecord::Schema.define(version: 2018_07_19_073353) do
 
   create_table "events", force: :cascade do |t|
     t.datetime "date"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2018_07_19_073046) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email"
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 User.create!(name: "Uy Binh",
   email: "binh@gmail.com",
   password: "foobar",
-  password_confirmation: "foobar",
+  password_confirmation: "foobar")
 
 
 User.create!(name: "Minh Xuan",
   email: "xuan@gmail.com",
   password: "foobar",
-  password_confirmation: "foobar",
+  password_confirmation: "foobar")
 
