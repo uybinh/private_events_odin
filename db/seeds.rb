2fb3680ff54095c8216d15c29b6ef949b5915859
Uy Binh <uybinh@gmail.com>
2018-07-19 14:31:18 +0700

    Add email to users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(name: "Uy Binh",
+  email: "binh@gmail.com",
+  password: "foobar",
+  password_confirmation: "foobar",
+
+
+User.create!(name: "Minh Xuan",
+  email: "xuan@gmail.com",
+  password: "foobar",
+  password_confirmation: "foobar",
+
