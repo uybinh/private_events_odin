ea15a339468951f83ae020cb8cd3e4b6218dc8fc
Uy Binh <uybinh@gmail.com>
2018-07-20 15:24:58 +0700

    Fix typo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   def full_title(page_title = "")
-    base_title = "Private Even App"
+    base_title = "Private Event App"
     page_title.empty? ? base_title : "#{page_title} - #{base_title}"
   end
 
