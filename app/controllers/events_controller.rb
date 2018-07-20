4885944a754f1c0ddf0a3b113a1f1a6e59641847
Uy Binh <uybinh@gmail.com>
2018-07-20 14:03:57 +0700

    Refactor pass upcoming with scope

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,12 +2,11 @@ class EventsController < ApplicationController
 
   def index
     @past = Event.past.includes(:creator)
-    @upcoming = Event.upcomping.includes(:creator)
+    @upcoming = Event.upcoming.includes(:creator)
   end
 
   def show
     @event = Event.find(params[:id])
   end
-
 
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -2,18 +2,9 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
   has_many :event_setups, foreign_key: "attended_event_id"
   has_many :attendees, through: :event_setups
+  scope :past, -> { where("date <= ?", Time.zone.now) }
+  scope :upcoming, -> { where("date <= ?", Time.zone.now) }
 
-
-  class << self
-    def past
-      where("date <= ?", Time.zone.now)
-    end
-
-    def upcomping
-      where("date > ?", Time.zone.now)
-    end
-
-  end
 
   def formatted_date
     date.strftime("%B %d, %Y")
