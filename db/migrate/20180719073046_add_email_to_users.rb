48682041406676c41942ee5adcfd99d6ac1c4565
Uy Binh <uybinh@gmail.com>
2018-07-20 14:16:18 +0700

    Edit migration

--- db/migrate/20180719073046_add_email_to_users.rb
+++ db/migrate/20180719073046_add_email_to_users.rb
@@ -2,5 +2,6 @@ class AddEmailToUsers < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :email, :string
     add_index :users, :email
+    add_column :users, :password_digest, :string
   end
 end
