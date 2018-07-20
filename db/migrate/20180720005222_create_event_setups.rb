57e78be732b9277c105439a88739b76cab48bb2d
Uy Binh <uybinh@gmail.com>
2018-07-20 14:34:41 +0700

    Fix migration

--- db/migrate/20180720005222_create_event_setups.rb
+++ db/migrate/20180720005222_create_event_setups.rb
@@ -1,8 +1,8 @@
 class CreateEventSetups < ActiveRecord::Migration[5.2]
   def change
     create_table :event_setups do |t|
-      t.references :attended_event, foreign_key: true
-      t.references :attendee, foreign_key: true
+      t.references :attended_event, foreign_key: { to_table: :events }
+      t.references :attendee, foreign_key: { to_table: :users }
 
       t.timestamps
     end
